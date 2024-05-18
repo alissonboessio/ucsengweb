41914fb784a9a94f69d18fb99a37b9ba96aded6a
alissonboessio <alissonboessio@gmail.com>
2024-05-18 10:50:42 -0300

    Acesso ao banco via ADO (insert, delete, select, update)

--- UCSCursoWebEng/UCSCursoWebEng/Controllers/HomeController.cs
+++ UCSCursoWebEng/UCSCursoWebEng/Controllers/HomeController.cs
@@ -3,19 +3,103 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UCSCursoWebEng.Models;
+using UCSCursoWebEng.Repositories;
 
 namespace UCSCursoWebEng.Controllers
 {
     public class HomeController : Controller
     {
+
+        private PessoaModel CriarPessoa(int i)
+        {
+            return
+                new PessoaModel(
+                    Guid.NewGuid().ToString(),
+                    $"Nome da pessoa {i}",
+                    $"Sobrenome da pessoa {i}"
+                );
+        }
+
+        private List<PessoaModel> CriarLista()
+        {
+            var r = new List<PessoaModel>();
+
+            for (int i = 0; i < 15; i++)
+            {
+                r.Add(CriarPessoa(i));
+            }
+
+            return r;
+        }
+
+
         public ActionResult Index()
         {
-            return View();
+
+            List<PessoaModel> listaDePessoas = new List<PessoaModel>();
+
+            PessoaADO pessoaADO = new PessoaADO();
+
+            listaDePessoas = pessoaADO.GetAll();
+
+            //var listaDePessoas = CriarLista();
+
+            
+            return View(listaDePessoas);
+        }
+
+        //quando nao esta definido, é tratado como Get.
+        //entao, passamos o id por url parameter
+        public ActionResult Detalhe(string id)
+        {
+            PessoaModel pessoa;
+
+            if (!String.IsNullOrEmpty(id))
+            {
+                PessoaADO pessoaADO = new PessoaADO();
+                pessoa = pessoaADO.GetById(id);
+
+            }
+            else
+            {
+                pessoa = new PessoaModel();
+            }
+
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public ActionResult Detalhe(PessoaModel pessoa)
+        {
+            PessoaADO pessoaADO = new PessoaADO();
+
+            if (string.IsNullOrEmpty(pessoa.Id))
+            {
+                pessoaADO.Inserir(pessoa);
+
+            }
+            else
+            {
+                pessoaADO.Atualizar(pessoa);
+
+            }
+
+
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Deletar(string id)
+        {
+            PessoaADO pessoa = new PessoaADO();
+
+            pessoa.Delete(id);
+            return RedirectToAction("Index");
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
---
+++ UCSCursoWebEng/UCSCursoWebEng/Models/PessoaModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UCSCursoWebEng.Models
+{
+    public class PessoaModel
+    {
+
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public String Sobrenome { get; set; }
+
+        public PessoaModel(string id, string nome, string sobrenome)
+        {
+            Id = id;
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+
+        public PessoaModel()
+        {
+            
+        }
+
+    }
+}
